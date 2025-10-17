d57963029744eaefe447bfe787ece3d650bacb14
Haseeb Sayed <ha741183@gmail.com>
2025-10-17 20:42:03 +0530

    Update pixel coordinates in Bot.py and add test.js for logging

--- Bot.py
+++ Bot.py
@@ -4,10 +4,11 @@ import keyboard
 import win32api
 import win32con
 
-#X:  535 Y:  459 RGB: (  0,   0,   0)
+#X:  726 Y:  397 RGB: (  0,   0,   0)
 #X:  625 Y:  428 RGB: (181, 183, 231)
 #X:  720 Y:  416 RGB: (180, 182, 231)
 #X:  807 Y:  421 RGB: (178, 181, 230)
+#These 4 comments above show the pixel value in x and y and their RGB value
 
 time.sleep(5)
 
@@ -20,12 +21,11 @@ def click(x, y):
 
     
 while keyboard.is_pressed('q') == False:
-    if pyautogui.pixel(535, 459)[0] == 0:
-        click(535,459)
-    if pyautogui.pixel(625, 428)[0] == 0:    
-        click(625, 428)
-    if pyautogui.pixel(720, 416)[0] == 0:    
-        click(720, 416)
-    if pyautogui.pixel(807, 421)[0] == 0:    
-        click(807, 421)      
-
+    if pyautogui.pixel(726, 397)[0] == 0:
+        click(726,397)
+    if pyautogui.pixel(876, 397)[0] == 0:    
+        click(876, 397)
+    if pyautogui.pixel(1026, 397)[0] == 0:    
+        click(1026, 397)
+    if pyautogui.pixel(1176, 397)[0] == 0:    
+        click(1176, 397)          
