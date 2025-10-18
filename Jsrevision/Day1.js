51941909aabeddf9413b9e08829a83064987ac63
Haseeb Sayed <ha741183@gmail.com>
2025-10-18 19:08:45 +0530

    Data type summary

---
+++ Jsrevision/Day1.js
@@ -0,0 +1,12 @@
+const accountId = 14453
+let accountEmail="haseeb@gmail.com"
+var accountpassword ="12345"
+accountcity ="pune"
+//accountId = 2
+accountEmail = "hc@.com"
+accountpassword = "67890"
+accountcity = "banglore"
+console.log(accountId);
+console.log("My name is haseeb")
+/*Try to avoid the var as it ccan cause cconfusion in large applications */
+console.table([accountId,accountEmail,accountpassword,accountcity])
